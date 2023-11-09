6a557e19d0f4f9117d1e0407cb30603867bbb4f2
osazee25 <osazeemarvins@yahoo.com>
2023-11-08 18:26:50 -0800

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -7,18 +7,28 @@ from datetime import datetime, timedelta
 class BaseModel:
     """The base class that other classes inherits from"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialization of the insatnce attributes
         
         Arguments:
-            id: the unique id of each user
-            created_at: the time the user was created
-            updated_at: the time the user was updated
+            - *args: list of arguments
+            - **kwargs: dict of key-values arguments
         """
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                            kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                            kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """The formated string method to overide printing to the stdoutput"""
